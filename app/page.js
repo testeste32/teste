23315ff89bde057360953084d920090ca8d5dbf9
testeste32 <joao.albolea@estudante.ufscar.br>
2025-04-17 14:27:58 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,54 +1,12 @@
 import ObjectDetection from "@/components/ObjectDetection";
-import { useState } from 'react';
 
-export default function ImageUpload() {
-  const [image, setImage] = useState(null);
-
-  const handleUpload = (e) => {
-    const file = e.target.files[0];
-    if (file) setImage(URL.createObjectURL(file));
-  };
-
+export default function Home() {
   return (
-    <div style={{ textAlign: 'center', padding: '20px' }}>
-      <h1>Upload de Imagem</h1>
-      
-      {/* Input de upload invisível */}
-      <input 
-        type="file" 
-        accept="image/*"
-        onChange={handleUpload}
-        id="upload-input"
-        style={{ display: 'none' }}
-      />
-      
-      {/* Botão personalizado */}
-      <label 
-        htmlFor="upload-input"
-        style={{
-          display: 'inline-block',
-          padding: '10px 20px',
-          background: '#0070f3',
-          color: 'white',
-          borderRadius: '5px',
-          cursor: 'pointer',
-          margin: '20px 0'
-        }}
-      >
-        Selecione uma imagem
-      </label>
-
-      {/* Preview da imagem */}
-      {image && (
-        <div>
-          <img 
-            src={image} 
-            alt="Preview" 
-            style={{ maxWidth: '100%', maxHeight: '300px', marginTop: '20px' }}
-          />
-        </div>
-      )}
-    </div>
+    <main className="flex min-h-screen flex-col items-center p-8 rounded-full">
+      <h1 className="gradient-title  rounded-md font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center">
+        Object Detection
+      </h1>
+      <ObjectDetection />
+    </main>
   );
 }
-  
