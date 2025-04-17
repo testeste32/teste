98fe94aabfc1b0af33af4df7fa463e7b8d13d9a7
testeste32 <joao.albolea@estudante.ufscar.br>
2025-04-17 13:53:46 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-8 rounded-full">
       <h1 className="gradient-title  rounded-md font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center">
-        Object Detection1111
+        Object D
       </h1>
       <ObjectDetection />
     </main>
