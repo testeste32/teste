44e3d2be7181486c4280c94e725c27eb5629ac94
kleber <kleber@debian.nome_dominio>
2025-04-17 16:10:50 -0300

    Adiciona página de upload e atualiza dependências (next)

---
+++ app/upload/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function UploadPage() {
+  return (
+    <div>
+      <h1>Página de Upload de Imagem</h1>
+      <button>Selecionar Imagem</button>
+      {/* Aqui você adicionará a lógica para o upload */}
+    </div>
+  );
+}
